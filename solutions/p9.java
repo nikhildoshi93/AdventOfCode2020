b65f9f9c4e57fd0ce0443796309ddb90b7937cc6
Nikhil Doshi <nikhil.doshi.93@gmail.com>
2021-01-14 20:46:34 +0530

    Update p9.java

--- solutions/p9.java
+++ solutions/p9.java
@@ -5,13 +5,42 @@ public class p9 {
 
     private static int preamble = 25;
 
-    private static boolean valuesAddUp(int val, HashMap<Integer, Integer> buffer) {
-        for (Integer i : buffer.keySet()) {
-            if (buffer.containsKey(val - i)) {
+    private static boolean valuesAddUp(int val, List<Integer> buffer) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (Integer i : buffer) {
+            if (!map.containsKey(i)) {
+                map.put(i, 1);
+            } else {
+                map.put(i, 2);
+            }
+        }
+
+        for (Integer i : map.keySet()) {
+            if (i*2 == val && map.get(i) == 2) {
+                return true;
+            } else if (map.containsKey(val - i)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static int findContiguousSum(int sum, List<String> in, int idx) {
+        int c = 0;
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = i + 1; j < idx; j++) {
+                Long temp = 0L;
+                List<Integer> range = new ArrayList<>();
+                for (int k = i; k <= j; k++) {
+                    temp += Integer.parseInt(in.get(k));
+                    range.add(Integer.parseInt(in.get(k)));
+                }
+                if (temp == sum) {
+                    return Collections.min(range) + Collections.max(range);
+                }
+            }
+        }
+        return -1;
     }
 
 	public static void main(String[] args) {
@@ -28,19 +57,20 @@ public class p9 {
             e.printStackTrace();
 	    }
 
-        HashMap<Integer, Integer> buffer = new HashMap<Integer, Integer>();
+        List<Integer> buffer = new ArrayList<>();
         for (int i = 0; i < preamble; i++) {
-            buffer.put(Integer.parseInt(in.get(i)), 1);
+            buffer.add(Integer.parseInt(in.get(i)));
         }
 
         for (int i = preamble; i < in.size(); i++) {
-            buffer.remove(Integer.parseInt(in.get(i - preamble)));
             int curr = Integer.parseInt(in.get(i));
             if (!valuesAddUp(curr, buffer)) {
                 System.out.println(curr);
+                System.out.println(findContiguousSum(curr, in, i));
                 return;
             }
-            buffer.put(curr, 1);
+            buffer.remove(0);
+            buffer.add(curr);
         }
 
         return;
